0fe4fc9d419dff10ede5ef0b7043765b5d7ce1de
WnOussama <oussamaouannassi@gmail.com>
2023-03-29 21:08:25 +0100

    [TECH] CM-12: Expose user id on authenticate

--- src/main/java/com/nexym/clinic/domain/user/UserServiceImpl.java
+++ src/main/java/com/nexym/clinic/domain/user/UserServiceImpl.java
@@ -69,15 +69,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public com.nexym.clinic.domain.user.model.auth.Authentication authenticate(LoginCredential loginCredential,
                                                                                AuthenticationManager authenticationManager) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginCredential.getEmail(),
+        var email = loginCredential.getEmail();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email,
                 loginCredential.getPassword(),
                 new ArrayList<>()));
-        var user = loadUserByUsername(loginCredential.getEmail());
+        com.nexym.clinic.domain.user.model.User userFromDb = getUserByEmail(email);
+        var user = loadUserByUsername(email);
         if (user != null) {
             var token = jwtUtils.generateToken(user);
             var expirationDate = jwtUtils.getExpirationDateFromToken(token);
             var now = new Date();
             return Authentication.builder()
+                    .id(userFromDb.getId())
                     .token(token)
                     .expiresIn((expirationDate.getTime() - now.getTime()) / 1000)
                     .build();
@@ -85,10 +88,14 @@ public class UserServiceImpl implements UserService {
         throw new AccessDeniedException("The access to this resource is denied");
     }
 
+    private User getUserByEmail(String email) {
+        return userPersistence.getUserByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException(String.format("User with email '%s' not found", email)));
+    }
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        com.nexym.clinic.domain.user.model.User user = userPersistence.getUserByEmail(email)
-                .orElseThrow(() -> new UserNotFoundException(String.format("User with email '%s' not found", email)));
+        com.nexym.clinic.domain.user.model.User user = getUserByEmail(email);
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 
