582aa3a80189c96cc55f3c84b62b4131bd521854
WnOussama <oussamaouannassi@gmail.com>
2023-04-27 17:14:54 +0100

    [FEAT] CM-15: implement mail sender

--- src/main/java/com/nexym/clinic/domain/user/mail/MailService.java
+++ src/main/java/com/nexym/clinic/domain/user/mail/MailService.java
@@ -2,6 +2,4 @@ package com.nexym.clinic.domain.user.mail;
 
 public interface MailService {
     String sendMail(MailDetail mailDetail);
-
-    String sendMailWithAttachment(MailDetail mailDetail);
 }
--- src/main/java/com/nexym/clinic/domain/user/mail/MailServiceImpl.java
+++ src/main/java/com/nexym/clinic/domain/user/mail/MailServiceImpl.java
@@ -1,17 +1,11 @@
 package com.nexym.clinic.domain.user.mail;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.util.Objects;
 
 @Service
 public class MailServiceImpl implements MailService {
@@ -46,39 +40,4 @@ public class MailServiceImpl implements MailService {
             return "Error while Sending email!!!";
         }
     }
-
-    @Override
-    public String sendMailWithAttachment(MailDetail mailDetail) {
-        // Creating a Mime Message
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper;
-
-        try {
-
-            // Setting multipart as true for attachment to be sent
-            mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-            mimeMessageHelper.setFrom(sender);
-            mimeMessageHelper.setTo(mailDetail.getRecipient());
-            mimeMessageHelper.setSubject(mailDetail.getSubject());
-            mimeMessageHelper.setText(mailDetail.getMsgBody());
-
-            // Adding the file attachment
-            FileSystemResource file = new FileSystemResource(new File(mailDetail.getAttachment()));
-
-            mimeMessageHelper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
-
-            // Sending the email with attachment
-            mailSender.send(mimeMessage);
-            return "Email has been sent successfully...";
-        }
-
-        // Catch block to handle the MessagingException
-        catch (MessagingException e) {
-
-            // Display message when exception is occurred
-            return "Error while sending email!!!";
-        }
-    }
-
-
 }
